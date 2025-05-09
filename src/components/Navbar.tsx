d3215ff5e8610052d6fa57451936d203ce353e19
zeeldadhaniya1895 <zeeldadhaniya1895@gmail.com>
2025-05-09 22:53:23 +0530

    " "

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,6 +14,7 @@ const Navbar = () => {
         setIsScrolled(false);
       }
     };
+    
 
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
